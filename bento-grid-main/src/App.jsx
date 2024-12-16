c29e17e214a74d16527d6043c6fdfb298b66bfee
macin <ernesto.dev24@gmail.com>
2024-12-15 20:01:32 -0600

    Bento Grid Project - Frontend Mentor

---
+++ bento-grid-main/src/App.jsx
@@ -0,0 +1,12 @@
+import SocialMedia from "./components/SocialMedia.jsx";
+
+import "./index.module.scss";
+import "./color.module.scss";
+
+function App (){
+ return (
+     <SocialMedia />
+ )
+}
+
+export default App;
---
+++ bento-grid-main/src/components/SocialMedia.jsx
@@ -0,0 +1,11 @@
+import "./SocialMedia.module.scss";
+
+function SocialMedia() {
+    return (
+        <div className="social-media">
+            <h1>Social Media <span className="social-media-number">10X</span> Faster with AI</h1>
+        </div>
+    )
+}
+
+export default SocialMedia;

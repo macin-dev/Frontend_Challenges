a31b5810a4e07b29bb7bea91116505d355ee4f7a
macin <ernesto.dev24@gmail.com>
2024-12-19 20:45:22 -0600

    Bento Grid Project - Frontend Mentor - Final version

--- bento-grid-main/src/components/ContentQuicker.jsx
+++ bento-grid-main/src/components/ContentQuicker.jsx
@@ -3,7 +3,7 @@ import styles from './ContentQuicker.module.scss';
 function ContentQuicker() {
     return (
         <section className={styles.contentSection}>
-            <h2 className={styles.contentTitle}>Create and schedule content <span className={styles.contentSubTitle}>quicker</span></h2>
+            <h2 className={styles.contentTitle}>Create and schedule content <span className={styles.contentSubTitle}>quicker.</span></h2>
             <img src="/src/assets/images/illustration-create-post.webp" alt="create post" />
         </section>
     )
--- bento-grid-main/src/components/FasterAudience.jsx
+++ bento-grid-main/src/components/FasterAudience.jsx
@@ -3,7 +3,7 @@ import styles from "./FasterAudience.module.scss";
 function FasterAudience() {
     return (
         <section className={styles.audienceSection}>
-            <h2 className={styles.audienceGrowth}>>56%</h2>
+            <h2 className={styles.audienceGrowth}> &gt;56%</h2>
             <span className={styles.audienceText}>faster audience growth</span>
             <img src="/src/assets/images/illustration-audience-growth.webp" alt="Audience growth" />
         </section>

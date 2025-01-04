f27efb46cf5fac0c2fd4623533df15e6409c4a1d
macin <ernesto.dev24@gmail.com>
2025-01-04 10:55:44 -0600

    Psycho Bunny Carousel - add emblacarousel dots

--- add-to-bag/src/components/carousel/Carousel.jsx
+++ add-to-bag/src/components/carousel/Carousel.jsx
@@ -1,14 +1,29 @@
 import useEmblaCarousel from "embla-carousel-react";
 import CarouselContainer from "./CarouselContainer";
+import { DotButton, useDotButtons } from "./EmblaCarouselDotButtons";
 import "./Carousel.css";
 
 const Carousel = () => {
-  const [emblaRef] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButtons(emblaApi);
 
   return (
     <div className="embla" ref={emblaRef}>
       <div className="embla__container">
         <CarouselContainer />
+      </div>
+
+      <div className="embla__dots">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            key={index}
+            onClick={() => onDotButtonClick(index)}
+            className={"embla__dot".concat(
+              index === selectedIndex ? " embla__dot--selected" : ""
+            )}
+          />
+        ))}
       </div>
     </div>
   );
